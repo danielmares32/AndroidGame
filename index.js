474d850c48ea50c78a3c2104040544f1185cb695
Daniel <danielmares32@gmail.com>
2021-11-06 21:28:49 -0600

    Se agrega websockets

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const socket = require('socket.io');
 var express = require('express'),
     app = express(),
     session = require('express-session');
@@ -38,5 +39,49 @@ app.post('/content', auth, function (req, res) {
     res.send("You can only see this after you've logged in.");
 });
 
-app.listen(3000);
+var server = app.listen(3000);
 console.log("app running at http://localhost:3000");
+
+var io = socket(server);
+//socket.io connection for the chat
+io.on('connection', auth, (socket)=>{
+  var user = '';
+  console.log("New chat connection: "+ socket.id);
+  socket.on('chat', (data)=>{
+    let chat_data = JSON.parse(data);
+    let user = chat_data.user
+    let chatId = chat_data.chatId;
+    socket.join(`${chatId}`);
+    console.log(`Username : ${user} joined Chat ID : ${chatId}`);
+
+    io.to(`${chatId}`).emit('newUserToChatRoom',user);
+
+  });
+
+  socket.on('leave', (data)=>{
+    let chat_data = JSON.parse(data);
+    let user = chat_data.user;
+    let chatId = chat_data.chatId;
+
+    console.log(`Username : ${user} leaved Room Name : ${chatId}`);
+    socket.broadcast.to(`${chatId}`).emit('userLeftChatRoom',user);
+    socket.leave(`${chatId}`);
+  })
+
+  socket.on('newMessage',(data)=> {
+    let messageData = JSON.parse(data)
+    let messageContent = messageData.messageContent
+    let chatId = messageData.chatId
+
+    console.log(`[Room Number ${chatId}] ${user} : ${messageContent}`)
+    
+    // Just pass the data that has been passed from the writer socket
+    const chatData = {
+        user : user,
+        messageContent : messageContent,
+        chatId : chatId
+    }
+    socket.broadcast.to(`${chatId}`).emit('updateChat',JSON.stringify(chatData)) // Need to be parsed into Kotlin object in Kotlin
+  })
+
+}); 
