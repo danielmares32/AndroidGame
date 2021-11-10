f567e733447d4ae85744cfbdf576d86789f9f24e
krure <71852395+krure@users.noreply.github.com>
2021-11-10 17:18:21 -0600

    Add files via upload

---
+++ src/main/java/com/example/chat/AdaptadorPersonaje.kt
@@ -0,0 +1,41 @@
+package com.example.guesswho
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+data class Personaje (val Foto:String,val Nom:String)
+class AdaptadorPersonaje(private val datos:MutableList<Personaje>,
+                         private val clickListener : (Personaje)->Unit ): RecyclerView.Adapter<AdaptadorPersonaje.PersonajesViewHolder>() {
+    class PersonajesViewHolder(val item: View):RecyclerView.ViewHolder(item) {
+        val lblnom=item.findViewById(R.id.Nombre)as TextView
+        val Foto= item.findViewById(R.id.PImagen)as ImageView
+       // val lbltel = item.findViewById(R.id.Tel)as TextView
+        fun bindPersonaje (titular: Personaje){
+            lblnom.text=titular.Nom
+            Foto.setImageResource(titular.Foto.toInt())
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PersonajesViewHolder {
+        val item= LayoutInflater.from(parent.context)
+            .inflate(R.layout.listitem_personaje,parent,false)as LinearLayout
+        return PersonajesViewHolder(item)
+
+    }
+
+    override fun onBindViewHolder(holder: PersonajesViewHolder, position: Int) {
+        val titular = datos[position]
+        holder.bindPersonaje(titular)
+        holder.item.setOnClickListener{clickListener(titular)}
+    }
+
+    override fun getItemCount(): Int {
+        return datos.size
+    }
+
+}
---
+++ src/main/java/com/example/chat/gameActivity.kt
@@ -0,0 +1,55 @@
+package com.example.guesswho
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var person: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        person = findViewById(R.id.Personaje)
+
+        val datos = MutableList(1){
+            Personaje(R.drawable.chrono.toString(),"Chrono")
+        }
+        datos.add(Personaje(R.drawable.ashketchum.toString(),"Ash Ketchum"))
+        datos.add(Personaje(R.drawable.bigboss.toString(),"Big Boss (Snake)"))
+        datos.add(Personaje(R.drawable.hunter.toString(),"Cazador"))
+        datos.add(Personaje(R.drawable.sumire.toString(),"Sumire"))
+        datos.add(Personaje(R.drawable.vergil.toString(),"Vergil"))
+        datos.add(Personaje(R.drawable.yoshi.toString(),"Yoshi"))
+        datos.add(Personaje(R.drawable.vader.toString(),"Vader"))
+        datos.add(Personaje(R.drawable.pacman.toString(),"Pacman"))
+        datos.add(Personaje(R.drawable.masterchief.toString(),"Master Chief"))
+        datos.add(Personaje(R.drawable.mario.toString(),"Mario"))
+        datos.add(Personaje(R.drawable.lady_maria_bloodborne.toString(),"Lady Maria"))
+        datos.add(Personaje(R.drawable.joker_persona5.toString(),"Joker"))
+        datos.add(Personaje(R.drawable.kratos.toString(),"Kratos"))
+        datos.add(Personaje(R.drawable.a2_nier_automata.toString(),"A2"))
+        datos.add(Personaje(R.drawable.batman.toString(),"Batman"))
+        datos.add(Personaje(R.drawable.dva.toString(),"D.Va"))
+        datos.add(Personaje(R.drawable.ghost.toString(),"Ghost"))
+        datos.add(Personaje(R.drawable.gwen.toString(),"Gwen"))
+        datos.add(Personaje(R.drawable.haru_persona5.toString(),"Haru"))
+        datos.add(Personaje(R.drawable.jinx.toString(),"Powder"))
+
+        val adaptador= AdaptadorPersonaje(datos){
+            //val intent = Intent (MainActivity@this,)
+            Toast.makeText(this,"Personaje escogido: ${it.Nom}",Toast.LENGTH_SHORT).show()
+        }
+        person.setHasFixedSize(true)
+        person.layoutManager=
+            GridLayoutManager(this,3)
+        person.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+        person.addItemDecoration(DividerItemDecoration(this,DividerItemDecoration.HORIZONTAL))
+        person.adapter=adaptador
+
+    }
+}
