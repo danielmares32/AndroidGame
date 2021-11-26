c1dda217c94292e62119e20a7f80e78794a68ab0
krure <71852395+krure@users.noreply.github.com>
2021-11-26 17:49:12 -0600

    Add files via upload

--- src/main/java/com/example/chat/AdaptadorPersonaje.kt
+++ src/main/java/com/example/chat/AdaptadorPersonaje.kt
@@ -1,6 +1,5 @@
 package com.example.chat
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,21 +8,39 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
-import com.example.chat.R
+import com.wajahatkarim3.easyflipview.EasyFlipView
+
 //import jdk.javadoc.internal.doclets.toolkit.util.DocPath.parent
 
 
 
 
 data class Personaje (val Foto:String,val Nom:String)
+
 class AdaptadorPersonaje(private val datos:MutableList<Personaje>,
                          private val clickListener : (Personaje)->Unit ): RecyclerView.Adapter<AdaptadorPersonaje.PersonajesViewHolder>() {
+    lateinit var mClickListener: ItemClickListener
 
-    class PersonajesViewHolder(val item: View):RecyclerView.ViewHolder(item) {
-        val lblnom=item.findViewById(R.id.Nombre)as TextView
-        val Foto= item.findViewById(R.id.PImagen)as ImageView
-       // val lbltel = item.findViewById(R.id.Tel)as TextView
+
+    public  class PersonajesViewHolder:RecyclerView.ViewHolder, View.OnClickListener {
+
+       lateinit var  itemc:View
+       lateinit var  lblnom:TextView
+        lateinit var Foto: ImageView
+        lateinit var myEasyFlipView:EasyFlipView
+
+        constructor(item: View) : super(item) {
+            itemc = item
+            lblnom=itemc.findViewById(R.id.Nombre)as TextView
+            Foto= itemc.findViewById(R.id.PImagen)as ImageView
+            myEasyFlipView=item.findViewById(R.id.flipC)
+            item.setOnClickListener(this)
+        }
+
+
+
+
+        // val lbltel = item.findViewById(R.id.Tel)as TextView
         fun bindPersonaje (titular: Personaje){
             lblnom.text=titular.Nom
 
@@ -32,14 +49,19 @@ class AdaptadorPersonaje(private val datos:MutableList<Personaje>,
                .into(Foto)
             //Foto.setImageResource(titular.Foto.toInt())
         }
+
+
+       override fun onClick(view: View?) {
+           myEasyFlipView.flipTheView()
+
+       }
+
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PersonajesViewHolder {
-
-
         val item= LayoutInflater.from(parent.context)
             .inflate(R.layout.listitem_personaje,parent,false)as LinearLayout
-
         return PersonajesViewHolder(item)
 
     }
@@ -47,11 +69,19 @@ class AdaptadorPersonaje(private val datos:MutableList<Personaje>,
     override fun onBindViewHolder(holder: PersonajesViewHolder, position: Int) {
         val titular = datos[position]
         holder.bindPersonaje(titular)
-        holder.item.setOnClickListener{clickListener(titular)}
+      //  holder.itemc.setOnClickListener{clickListener(titular)}
     }
 
     override fun getItemCount(): Int {
         return datos.size
+    }
+
+    fun setClickListener(it:ItemClickListener){
+        this.mClickListener=it
+    }
+
+   public interface ItemClickListener {
+         fun onItemClick(view: View?, position:Int);
     }
 
 }
