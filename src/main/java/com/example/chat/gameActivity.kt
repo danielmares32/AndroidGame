770e39c6f3525123d79cc466ba43531461e5d912
Daniel <danielmares32@gmail.com>
2021-11-29 12:20:08 -0600

    Se agrega el dialog de mi personaje

--- src/main/java/com/example/chat/gameActivity.kt
+++ src/main/java/com/example/chat/gameActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.Toast
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.GridLayoutManager
@@ -12,10 +13,26 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.chat.R
 import org.json.JSONException
 import org.json.JSONObject
+import android.content.DialogInterface
+import android.app.AlertDialog
+import android.graphics.drawable.Drawable
+
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+
 
 class gameActivity : AppCompatActivity() {
     private lateinit var person: RecyclerView
     private lateinit var chat: ChatActivity
+    private lateinit var miPersonaje: Button
+    private lateinit var username: String
+    private lateinit var chatId: String
     private val mSocket = SocketHandler.getSocket()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,22 +40,25 @@ class gameActivity : AppCompatActivity() {
         setContentView(R.layout.game_activity)
         person = findViewById(R.id.Personaje)
         chat = findViewById(R.id.chat)
+        miPersonaje = findViewById(R.id.miPersonaje)
 
-        val username: String? = intent.getStringExtra("username")
-        val chatId: String? = intent.getStringExtra("roomId")
-        chat.setUserName("Rival: "+username!!)
+        username = intent.getStringExtra("username")!!
+        chatId = intent.getStringExtra("roomId")!!
+        chat.setUserName("Rival: "+username)
 
         chat.getLblsend().setOnClickListener {
             if(chat.getMensaje().text.toString() != ""){
-                chat.getChatList().add(Message(username,chat.getMensaje().text.toString(),chatId!!,0))
+                chat.getChatList().add(Message(username,chat.getMensaje().text.toString(),chatId,0))
                 val adapter = ChatAdapter(this, chat.getChatList())
-                //chat.getRecycler().setHasFixedSize(true)
                 chat.getRecycler().layoutManager = LinearLayoutManager(this)
                 chat.getRecycler().adapter = adapter
                 chat.getRecycler().scrollToPosition(chat.getChatList().size - 1)
                 enviarMensajes()
                 chat.getMensaje().text = ""
             }
+        }
+        miPersonaje.setOnClickListener {
+            getMiPersonajeApi()
         }
         SocketHandler.setSocket()
         SocketHandler.establishConnection()
@@ -109,9 +129,6 @@ class gameActivity : AppCompatActivity() {
 
     private fun enviarMensajes(){
         print("Entre a enviar mensaje")
-        /*SocketHandler.setSocket()
-        SocketHandler.establishConnection()
-        val mSocket = SocketHandler.getSocket()*/
 
         val data: JSONObject = JSONObject()
         try {
@@ -126,5 +143,72 @@ class gameActivity : AppCompatActivity() {
         mSocket.emit("newMessage", data)
 
 
+    }
+
+    private fun getMiPersonaje(personaje: String){
+        val alertadd: AlertDialog.Builder = AlertDialog.Builder(this)
+        val factory = LayoutInflater.from(this)
+        val view: View = factory.inflate(R.layout.mi_personaje_dialog, null)
+        val nombreMiPersonaje: TextView = view.findViewById(R.id.txtPersonaje)
+        val imageMiPersonaje: ImageView = view.findViewById(R.id.PImagen)
+        print(R.drawable.yoshi.toString())
+        imageMiPersonaje.background = when(personaje){
+            "ash"->ResourcesCompat.getDrawable(resources, R.drawable.ashketchum, null)
+            "batman"->ResourcesCompat.getDrawable(resources, R.drawable.batman, null)
+            "bigboss"->ResourcesCompat.getDrawable(resources, R.drawable.bigboss, null)
+            "chrono"->ResourcesCompat.getDrawable(resources, R.drawable.chrono, null)
+            "cortana_halo"->ResourcesCompat.getDrawable(resources, R.drawable.cortana_halo, null)
+            "dva"->ResourcesCompat.getDrawable(resources, R.drawable.dva, null)
+            "futaba_p5"->ResourcesCompat.getDrawable(resources, R.drawable.futaba_p5, null)
+            "ghost"->ResourcesCompat.getDrawable(resources, R.drawable.ghost, null)
+            "gwen"->ResourcesCompat.getDrawable(resources, R.drawable.gwen, null)
+            "haru_p5"->ResourcesCompat.getDrawable(resources, R.drawable.haru_p5, null)
+            "hunter"->ResourcesCompat.getDrawable(resources, R.drawable.hunter, null)
+            "jinx"->ResourcesCompat.getDrawable(resources, R.drawable.jinx, null)
+            "joker_p5"->ResourcesCompat.getDrawable(resources, R.drawable.joker_p5, null)
+            "kasumi_p5"->ResourcesCompat.getDrawable(resources, R.drawable.kasumi_p5, null)
+            "kratos"->ResourcesCompat.getDrawable(resources, R.drawable.kratos, null)
+            "mario"->ResourcesCompat.getDrawable(resources, R.drawable.mario, null)
+            "masterchief"->ResourcesCompat.getDrawable(resources, R.drawable.masterchief, null)
+            "pacman"->ResourcesCompat.getDrawable(resources, R.drawable.pacman, null)
+            "samus"->ResourcesCompat.getDrawable(resources, R.drawable.samus, null)
+            "steve"->ResourcesCompat.getDrawable(resources, R.drawable.steve, null)
+            "toad"->ResourcesCompat.getDrawable(resources, R.drawable.toad, null)
+            "vader"->ResourcesCompat.getDrawable(resources, R.drawable.vader, null)
+            "yoshi"->ResourcesCompat.getDrawable(resources, R.drawable.yoshi, null)
+            else -> ResourcesCompat.getDrawable(resources, R.drawable.yoshi, null)
+        }
+        nombreMiPersonaje.text = personaje
+        alertadd.setView(view)
+        alertadd.show()
+    }
+
+    private fun getMiPersonajeApi(){
+        val url: String = "http://10.0.2.2:3000/getPersonaje"
+        val requestQueue = Volley.newRequestQueue(this)
+        val postData: JSONObject = JSONObject()
+        try {
+            postData.put("usuario",username)
+            postData.put("chatId", chatId)
+        } catch (e: JSONException){
+            Log.d("Error",e.toString())
+        }
+
+
+        Log.d("Data", postData.toString())
+
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.POST,
+            url,
+            postData,
+            { response ->
+                println(response)
+                println(JSONObject(response.toString()).get("personaje"))
+                val personaje = JSONObject(response.toString()).get("personaje")
+                getMiPersonaje(personaje.toString())
+            }
+        ) { error -> error.printStackTrace() }
+
+        requestQueue.add(jsonObjectRequest)
     }
 }
