9f4dc6add01b232b4d4fc91e0987b3d92860ae91
EnmaDiaz3 <84469188+EnmaDiaz3@users.noreply.github.com>
2021-11-30 12:46:27 -0600

    Add files via upload

---
+++ src/main/java/com/example/chat/pantalla_victoria.kt
@@ -0,0 +1,18 @@
+package com.example.chat
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class pantalla_victoria : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla_victoria)
+        val continuar = findViewById<TextView>(R.id.finalizar)
+        continuar.setOnClickListener {
+            val intent= Intent(this,menu_activity::class.java)
+            startActivity(intent)
+        }
+    }
+}
